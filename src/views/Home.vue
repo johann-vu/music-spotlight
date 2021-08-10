1d415bdafa310c4ae24960bd83861acd8cff4897
johann-vu <johann.vu@live.de>
2021-08-10 15:03:50 +0200

    Creating a temporary Homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="home__start">
+      <span class="home__title">Your<br />Music<br />Spotlight</span>
+      <a class="home__auth-link">Let's go!</a>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+};
 </script>
+
+<style scoped>
+
+.home__start {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  height: 100vh;
+}
+
+.home__auth-link {
+  font-size: 16pt;
+  font-weight: bold;
+}
+.home__auth-link:hover {
+  cursor: pointer;
+  text-decoration: underline black;
+}
+
+.home__title {
+  font-weight: bold;
+  font-size: 72pt;
+  line-height: 100%;
+}
+</style>
