d70705eae95ac91a087e1851cc458835cc33f9e8
johann-vu <johann.vu@live.de>
2021-08-12 19:14:38 +0200

    Refactoring

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,16 +1,27 @@
 <template>
   <div class="footer__wrapper">
-    <a href="https://github.com/johann-vu/music-spotlight">
-      <img class="footer__github-icon" src="@/assets/GitHub-Mark-32px.png" />
+    <a
+      class="footer__icon-wrapper"
+      href="https://github.com/johann-vu/music-spotlight"
+    >
+      <img class="footer__icon" src="@/assets/GitHub-Mark-64px.png" />
     </a>
   </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 .footer__wrapper {
   width: 100%;
   display: flex;
   justify-content: center;
-  padding: 10px 0;
+  padding: 20px 0;
+
+  .footer__icon-wrapper {
+      margin: 0 5px;
+    .footer__icon {
+      height: 32px;
+      width: 32px;
+    }
+  }
 }
 </style>
