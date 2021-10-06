ef9bd2ceab344b1a233a35757e369611a047624b
johann-vu <68650868+johann-vu@users.noreply.github.com>
2021-10-06 19:52:33 +0200

    Moved text to translation file
    Refactored the Acknowledgements

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,17 +1,34 @@
 <template>
   <div class="about">
-    <h1>About "Your Music Spotlight"</h1>
-    <h2>Privacy</h2>
-    <p>
-    This application does not save any of your data to a serve. Everything is processed on your own device.
-    </p>
-    <p>
-    If you want to revoke the permissions, go to accounts.spotify.com and remove "Music Spotlight" from the list of apps.
-    </p>
-    <h2>Acknowledgements</h2>
+    <h1>{{ $t("about_title") }}</h1>
+    <h2>{{ $t("privacy_headline") }}</h2>
+    <p>{{ $t("about_data_paragraph") }}</p>
+    <p>{{ $t("about_permissions_paragraph") }}</p>
+    <h2>{{ $t("acknowledgements_headline") }}</h2>
     <ul>
-      <li>Icons from https://tablericons.com under the MIT License</li>
-      <li>Node-Vibrant from https://github.com/Vibrant-Colors/node-vibrant under the MIT License</li>
+      <li v-for="ack in acknowledgements" :key="ack.title">
+        <a :href="ack.url">{{ ack.title }}</a> ({{ack.license}})
+      </li>
     </ul>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      acknowledgements: [
+        {
+          title: "Tabler Icons",
+          url: "https://tablericons.com",
+          license: "MIT License",
+        },
+        {
+          title: "Node-Vibrant",
+          url: "https://github.com/Vibrant-Colors/node-vibrant",
+          license: "MIT License",
+        }
+      ],
+    };
+  },
+};
+</script>
