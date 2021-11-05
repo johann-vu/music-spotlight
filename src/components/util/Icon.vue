678ae1b54324e76d79af775b291ae0d878666303
johann-vu <68650868+johann-vu@users.noreply.github.com>
2021-11-05 11:16:23 +0100

    Adding reusable icon component

---
+++ src/components/util/Icon.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="icon_wrapper">
+    <img class="--dark" :src="lightIconPath" />
+    <img class="--light" :src="darkIconPath" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Icon",
+  computed: {
+    lightIconPath() {
+      return `/assets/${this.name}-light.svg`;
+    },
+    darkIconPath() {
+      return `/assets/${this.name}-dark.svg`;
+    },
+  },
+  props: {
+    name: String,
+  },
+};
+</script>
+
+<style scoped>
+img {
+    height: inherit;
+}
+.--light {
+  display: var(--dark-mode-hidden);
+}
+.--dark {
+  display: var(--dark-mode-visible);
+}
+</style>
--- src/views/Error.vue
+++ src/views/Error.vue
@@ -3,14 +3,24 @@
     <div class="error_container">
       <h1 class="error_title">{{$t("error_title")}}</h1>
       <p class="error_descr">{{$t("error_description")}}</p>
+      <div class="error_details-container">
+        <p class="error_details-headline">
+          <Icon name="chevron-right" class="error_details-chevron"/>
+          {{$t("error_details-headline")}}
+          </p>
+      </div>
       <router-link to="/">{{$t("error_link")}}</router-link>
     </div>
   </div>
 </template>
 
 <script>
+import Icon from '../components/util/Icon.vue'
 export default {
   name: "ErrorView",
+  components: {
+    Icon
+  }
 };
 </script>
 
@@ -21,6 +31,19 @@ export default {
   justify-content: center;
   .error_container {
     max-width: 500px;
+    .error_details-container {
+      opacity: 0.5;
+      .error_details-headline {
+        display: flex;
+        align-items: center;
+        &:hover {
+          cursor: pointer;
+        }
+        .error_details-chevron {
+          height: 15px;
+        }
+      }
+    }
   }
 }
 </style>
