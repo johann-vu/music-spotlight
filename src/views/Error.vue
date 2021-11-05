cc7f019c566e13a2ee8c7689b907f0d10ea153ea
johann-vu <68650868+johann-vu@users.noreply.github.com>
2021-11-05 11:36:32 +0100

    Show error details

--- src/views/Error.vue
+++ src/views/Error.vue
@@ -1,25 +1,39 @@
 <template>
   <div class="error_wrapper">
     <div class="error_container">
-      <h1 class="error_title">{{$t("error_title")}}</h1>
-      <p class="error_descr">{{$t("error_description")}}</p>
+      <h1 class="error_title">{{ $t("error_title") }}</h1>
+      <p class="error_descr">{{ $t("error_description") }}</p>
       <div class="error_details-container">
-        <p class="error_details-headline">
-          <Icon name="chevron-right" class="error_details-chevron"/>
-          {{$t("error_details-headline")}}
-          </p>
+        <p class="error_details-headline" @click="toggleDetails()">
+          <Icon name="chevron-right" class="error_details-chevron" />
+          {{ $t("error_details-headline") }}
+        </p>
+        <p v-show="detailsOpen" class="error_details-content">
+          {{details}}
+        </p>
       </div>
-      <router-link to="/">{{$t("error_link")}}</router-link>
+      <router-link to="/">{{ $t("error_link") }}</router-link>
     </div>
   </div>
 </template>
 
 <script>
-import Icon from '../components/util/Icon.vue'
+import Icon from "../components/util/Icon.vue";
 export default {
   name: "ErrorView",
   components: {
-    Icon
+    Icon,
+  },
+  data() {
+    return {
+      details: this.$route.query.error,
+      detailsOpen: false,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsOpen = !this.detailsOpen
+    }
   }
 };
 </script>
